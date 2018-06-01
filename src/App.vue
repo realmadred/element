509c30a7cd732db56468f485658b50b8a093bced
liufeng <liufeng@hongte.info>
2018-06-01 10:19:43 +0800

    vue init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,111 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <ButtonTest/>
+
+    <el-switch
+      v-model="value3"
+      active-text="按月付费"
+      inactive-text="按年付费">
+    </el-switch>
+    <el-switch
+      style="display: block"
+      v-model="value4"
+      active-color="#13ce66"
+      inactive-color="#ff4949"
+      active-text="按月付费"
+      inactive-text="按年付费">
+    </el-switch>
+
+    <div class="block">
+      <span class="demonstration">默认</span>
+      <el-date-picker
+        v-model="value1"
+        type="datetime"
+        placeholder="选择日期时间">
+      </el-date-picker>
+    </div>
+    <div class="block">
+      <span class="demonstration">带快捷选项</span>
+      <el-date-picker
+        v-model="value2"
+        type="datetime"
+        placeholder="选择日期时间"
+        align="right"
+        :picker-options="pickerOptions1">
+      </el-date-picker>
+    </div>
+    <div class="block">
+      <span class="demonstration">设置默认时间</span>
+      <el-date-picker
+        v-model="time3"
+        type="datetime"
+        placeholder="选择日期时间"
+        default-time="12:00:00">
+      </el-date-picker>
+    </div>
+
+    <el-progress :percentage="80" color="#8e71c7"></el-progress>
+    <el-progress :percentage="100" :stroke-width="18"  status="success"></el-progress>
+    <el-progress type="circle" :percentage="100" status="success"></el-progress>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+  import HelloWorld from './components/HelloWorld'
+  import ButtonTest from './components/ButtonTest'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld,
+      ButtonTest
+    },
+    data() {
+      return {
+        value3: true,
+        value4: true,
+        pickerOptions1: {
+          shortcuts: [{
+            text: '今天',
+            onClick(picker) {
+              picker.$emit('pick', new Date());
+            }
+          }, {
+            text: '昨天',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24);
+              picker.$emit('pick', date);
+            }
+          }, {
+            text: '一周前',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', date);
+            }
+          }]
+        },
+        value1: '',
+        value2: '',
+        time3: ''
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .block{
+    display: inline-block;
+    padding: 60px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+import { Button, Select,Row,Switch,DatePicker,Progress } from 'element-ui';
 import App from './App'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
+// Vue.use(ElementUI);
+Vue.use(Progress);
+Vue.use(DatePicker);
+Vue.use(Switch);
+Vue.use(Row);
+Vue.use(Button) ;
+Vue.use(Select);
+/* eslint-disabl;e no-new */
 new Vue({
   el: '#app',
   components: { App },
   template: '<App/>'
-})
+});
